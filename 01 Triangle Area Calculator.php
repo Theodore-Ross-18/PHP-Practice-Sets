4a89e66207684f070f30802c9fcf669fd89301f2
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2025-02-18 06:27:09 +0800

    CHANGES Comment

--- 01 Triangle Area Calculator.php
+++ 01 Triangle Area Calculator.php
@@ -1,10 +1,10 @@
-// Practice Set 1: Triangle Area Calculator
+<!-- Practice Set 1: Triangle Area Calculator
 
-// A Simple PHP script that calculates the area of a triangle based
-// on the lengths of its three sides using Heron's Formula.
+A Simple PHP script that calculates the area of a triangle based
+on the lengths of its three sides using Heron's Formula.
 
-// area_of_triangle = sqrt(s * (s - side1) * (s - side2) * (s - side3))
-// where s = side1 + side2 + side3 / 2
+area_of_triangle = sqrt(s * (s - side1) * (s - side2) * (s - side3))
+where s = side1 + side2 + side3 / 2 -->
 
 <?php
 function squareRoot($num) {
