f655ba585319d61826c967eb3eb29b681fafb6f3
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2025-02-18 05:35:27 +0800

    Tests

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+
+// Lesson 1
+
+echo 'Hello';
+echo 1234;
+echo '<h1>Hello, World!</h1>';
+
+print_r('Hello');
+print_r([1,2,3]);
+
+class Person {
+    public $name;
+    public $age;
+}
+
+$person = new Person();
+$person->name = "Alice";
+$person->age = 25;
+
+var_dump('Hello');
+var_dump([1,2,3]);
+
+// Lesson 2
+
+$string = 'string';
+$integer = 1234;
+$float = 1234.5;
+$boo = true;
+$lean = false;
+$array = ['string', 1234, 1234.5, true, false];
+$null = null;
+// $resource = 
+
+?>
