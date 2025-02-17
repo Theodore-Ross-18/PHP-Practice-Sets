b8b1353209d4ead4a7c08934d5e6c12aede54919
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2025-02-18 06:31:53 +0800

    Practice Set 2

---
+++ 02 Array of Fruits.php
@@ -0,0 +1,5 @@
+<!-- Practice Set 2: Array of Fruits
+
+- An array of fruits with at least five elements.
+- With a PHP script that uses a for loop to display each element of the array in ordered list (ol).
+- Using count() from available array functions in the condition of the for loop. -->
