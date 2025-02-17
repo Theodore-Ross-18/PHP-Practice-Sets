1a97d9eb29e454b5dbe0ae71a5509ae18572309b
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2025-02-18 06:44:06 +0800

    Practice Set 3

---
+++ 03 Array of Even Numbers.php
@@ -0,0 +1,9 @@
+<!-- Practice Set 3: Array of Even Numbers
+
+- Write the PHP script that uses a while loop to display even number elements from the matrix below
+
+$matrix = [
+	[12, 23, 34],
+	[45, 55, 62],
+	[71, 84, 90]
+]; -->
