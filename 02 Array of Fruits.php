80cc98df93fe57201228ef4d68ea94ae2f6af0f2
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2025-02-18 06:33:40 +0800

    PHP HTML

--- 02 Array of Fruits.php
+++ 02 Array of Fruits.php
@@ -3,3 +3,19 @@
 - An array of fruits with at least five elements.
 - With a PHP script that uses a for loop to display each element of the array in ordered list (ol).
 - Using count() from available array functions in the condition of the for loop. -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fruit List</title>
+</head>
+<body>
+    
+</body>
+</html>
