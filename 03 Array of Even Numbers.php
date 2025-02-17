9637b5148816ecca561fb53fae1e1bda564062ea
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2025-02-18 06:51:22 +0800

    CONDITION While Matrix

--- 03 Array of Even Numbers.php
+++ 03 Array of Even Numbers.php
@@ -1,6 +1,6 @@
 <!-- Practice Set 3: Array of Even Numbers
 
-- Write the PHP script that uses a while loop to display even number elements from the matrix below
+- A PHP script that uses a while loop to display even number elements from the matrix below
 
 $matrix = [
 	[12, 23, 34],
@@ -25,6 +25,22 @@ $matrix = [
     <title>Even Numbers in Matrix</title>
 </head>
 <body>
-    
+<body class="container mt-5">
+    <h2 class="text-center">Even Numbers in Matrix</h2>
+    <ul class="list-group">
+        <?php
+        $row = 0;
+        while ($row < count($matrix)) {
+            $col = 0;
+            while ($col < count($matrix[$row])) {
+                if ($matrix[$row][$col] % 2 == 0) {
+                    echo "<li class='list-group-item'>" . $matrix[$row][$col] . "</li>";
+                }
+                $col++;
+            }
+            $row++;
+        }
+        ?>
+    </ul>
 </body>
 </html>
